d57052082d1ea2eb9eb8f43efe7e9a5bdf91266c
Nicky Kayyarath <nkayyarath@gmail.com>
2023-11-15 14:40:40 -0500

    initization of fruitful roots backend

---
+++ fruitfulroots/src/main/java/com/krath/fruitfulroots/FruitfulrootsApplication.java
@@ -0,0 +1,13 @@
+package com.krath.fruitfulroots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitfulrootsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitfulrootsApplication.class, args);
+	}
+
+}
---
+++ fruitfulroots/src/test/java/com/krath/fruitfulroots/FruitfulrootsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krath.fruitfulroots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitfulrootsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
