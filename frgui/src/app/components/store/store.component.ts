120e8a98646fb949e538b2dcf7587c0b2fdaf8aa
Nicky Kayyarath <nkayyarath@gmail.com>
2023-11-29 03:09:12 -0500

    building the store's component where it would hold the products, and have a sidebar with product categories

---
+++ frgui/src/app/components/store/store.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-store',
+  templateUrl: './store.component.html',
+  styleUrl: './store.component.css'
+})
+export class StoreComponent {
+
+}
